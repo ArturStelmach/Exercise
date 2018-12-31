7563ab942fa062853a1814368223bb510bfbcf8f
Artur Stelmach <arturserim@gmail.com>
2018-12-31 17:17:16 +0100

    Size show find max value

--- DoubleLinkedList/DoubleLinkedList/Source.cpp
+++ DoubleLinkedList/DoubleLinkedList/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <climits>
+#include <cstdlib>
 using namespace std;
 
 class Node{
@@ -20,21 +21,69 @@ public:
 		else {
 			this->next = NewNode;
 			NewNode->prev = this;
+			NewNode->next = nullptr;
 		}
 	}
 
 	void push_front(Node* NewNode) {
-		this->prev = NewNode;
-		NewNode->next = this;
+		NewNode->next = this->next;
+		NewNode->prev = this;
+		this->next = NewNode;
+		NewNode->next->prev = NewNode;
 	}
 
+	void show() {
+		cout << this->value << " ";
+		if (this->next != nullptr)
+			this->next->show();
 
+	}
+
+	void find_max(float& currentMax, Node*& current,int itr) {
+		
+		while (itr > 1) {
+			if (this->value > currentMax) {
+				current = this;
+				currentMax = this->value;
+			}
+			itr--;
+			this->next->find_max(currentMax, current,itr);
+			
+		}
+	}
+
+	int size(int n=0) {
+		if (this->next != nullptr) this->next->size(++n);
+		else return n;
+	}
+
+	void SortByValue() {
+		int amount = this->size();
+		Node* temp;
+		float val=-FLT_MAX;
+		for (Node* itr = this; itr->next != nullptr; itr = itr->next) {
+			val = -FLT_MAX;
+			find_max(val, temp, amount--);
+			temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			this->push_back(temp);
+			cout << endl;
+			this->show();
+		}
+	}
+
+	
 };
 
 
 int main() {
-
-
+	Node* root=new Node(0);
+	for (int i = 0; i < 10; i++) {
+		root->push_back(new Node((float)rand()));
+	}
+	root->show();
+	root->SortByValue();
+	root->show();
 
 	cin.ignore();
 	getchar();
